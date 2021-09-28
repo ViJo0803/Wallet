479faa18cc9daf023fb2e061d267d1b3c9a40ee0
LuMazzett <55467824+luchomazze@users.noreply.github.com>
2021-09-28 18:52:34 -0300

    Inicializacion de Componentes Register Init y Register Extended, Renderizacion en Landing

--- client/components/LandingPage/index.js
+++ client/components/LandingPage/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { View, Button, Text } from 'react-native';
-
+import RegisterInit from "../RegisterInit";
 
 function LandingPage({ navigation }) {
     return (
         <View>
             <Text>This is LandingPage</Text>
             <Button title="Go Home" onPress={() => navigation.navigate('Home')}/>
+            <RegisterInit/>
         </View>
     )
 
