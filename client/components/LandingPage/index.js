d8fc3a2e12236c31d0b4c404b07c2d028b163c82
Luciano-Fuertes <chiva.fuertes@gmail.com>
2021-09-28 17:36:25 -0300

    landing con botones, register creada

--- client/components/LandingPage/index.js
+++ client/components/LandingPage/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { View, Button, Text } from 'react-native';
+import { styles } from './styles'
 
 
 function LandingPage({ navigation }) {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>This is LandingPage</Text>
-            <Button title="Go Home" onPress={() => navigation.navigate('Home')}/>
+            <View style={styles.containerText}>
+                <View>
+                    <Button style={styles.button} title="Log In" onPress={() => navigation.navigate('Home')} />
+                </View>
+                <Text>New User?</Text>
+                <View>
+                    <Button style={styles.button} title="Register" onPress={() => navigation.navigate('Register')} />
+                </View>
+            </View>
         </View>
     )
 
---
+++ client/components/LandingPage/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        marginTop: '45%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'columns',
+    },
+    containerText: {
+        marginTop: '15%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'columns',
+    },
+    button: {
+        borderRadius: '60%',
+    },
+})
