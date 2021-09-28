b516cc2ad7dc7f85588762f15ca76607ba867f8b
Luciano-Fuertes <chiva.fuertes@gmail.com>
2021-09-28 14:44:45 -0300

    primer renderizado de landing-home

---
+++ client/components/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { View, Text } from 'react-native';
+
+function Home() {
+    return (
+        <View>
+            <Text>You're now Home</Text>
+        </View>
+    )
+}
+
+export default Home
