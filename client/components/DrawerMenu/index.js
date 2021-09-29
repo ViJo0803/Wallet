2b5d0953008f04b5fef9b1b6b2793a25c41fabd9
Luciano-Fuertes <chiva.fuertes@gmail.com>
2021-09-29 13:59:58 -0300

    drawerMenu created

---
+++ client/components/DrawerMenu/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { View, Text } from 'react-native';
+import { styles } from './styles'
+
+function DrawerMenu() {
+    return (
+        <View>
+            
+        </View>
+    )
+}
+
+export default DrawerMenu
