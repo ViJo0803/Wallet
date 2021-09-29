7a23b86129b84b0e2c8aa7e48df1c71087279294
Luciano-Fuertes <chiva.fuertes@gmail.com>
2021-09-29 15:45:10 -0300

    avance en drawer

--- client/components/DrawerMenu/index.js
+++ client/components/DrawerMenu/index.js
@@ -2,12 +2,41 @@ import React from 'react'
 
 import { View, Text } from 'react-native';
 import { styles } from './styles'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-function DrawerMenu() {
+const Stack = createStackNavigator();
+
+function Menu() {
     return (
-        <View>
+        <View style={styles.container}>
             
         </View>
+    )
+}
+
+function DrawerMenu(){
+    return(
+<NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='LandingPage'
+          component={LandingPage}
+          options={{
+            headerShown: false,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            fontSize: 30,
+          }} />
+        <Stack.Screen
+          name='Home'
+          component={Home} />
+        <Stack.Screen
+          name='Register'
+          component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
     )
 }
 
