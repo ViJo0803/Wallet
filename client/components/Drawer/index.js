ec1bf73953a72c6c9b07c78ecc7aec50f5193bdc
Luciano-Fuertes <chiva.fuertes@gmail.com>
2021-09-30 17:43:06 -0300

    error de guardado corregido

--- client/components/Drawer/index.js
+++ client/components/Drawer/index.js
@@ -1,16 +1,10 @@
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import LandingPage from '../LandingPage';
-<<<<<<< HEAD
-import Statistics from '../Statistics/Statistics';
-import UserProfile from '../UserProfile/index'
-import Home from '../Home/index'
-=======
 import Statistics from '../Statistics';
 import UserProfile from '../UserProfile/index'
 import TabNav from '../TabNav';
 import Accounts from '../Accounts';
->>>>>>> TabNav
 
 const Drawer = createDrawerNavigator();
 
@@ -20,25 +14,6 @@ const Drawer = createDrawerNavigator();
 function DrawerBar() {
 
     return (
-<<<<<<< HEAD
-        
-        
-           <Drawer.Navigator
-                initialRouteName="Home">
-                <Drawer.Screen name="Home" component={Home} />
-                <Drawer.Screen name="User Profile" component={UserProfile} />
-                {/* <Drawer.Screen name="Cuentas" component={Cuentas} /> */}
-                <Drawer.Screen name="Statistics" component={Statistics} />
-                <Drawer.Screen name="Log out" component={LandingPage} />
-                
-                
-            
-            </Drawer.Navigator> 
-
-        
-        
-        )
-=======
         <Drawer.Navigator
             initialRouteName="TabNav">
             <Drawer.Screen
@@ -51,7 +26,6 @@ function DrawerBar() {
             <Drawer.Screen name="Log out" component={LandingPage} />
         </Drawer.Navigator>
     )
->>>>>>> TabNav
 }
 
 export default DrawerBar
