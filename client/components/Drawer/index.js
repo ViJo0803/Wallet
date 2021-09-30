44ee07a5cea4492c8c74a1e90579976b92a00e26
Luciano-Fuertes <chiva.fuertes@gmail.com>
2021-09-30 14:57:46 -0300

    display componente provisional UserProfile

--- client/components/Drawer/index.js
+++ client/components/Drawer/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import LandingPage from '../LandingPage';
 import Statistics from '../Statistics/Statistics';
+import UserProfile from '../UserProfile/index'
 import Home from '../Home/index'
-
 
 const Drawer = createDrawerNavigator();
 
@@ -18,7 +18,7 @@ function DrawerBar() {
            <Drawer.Navigator
                 initialRouteName="Home">
                 <Drawer.Screen name="Home" component={Home} />
-                {/* <Drawer.Screen name="User Profile" component={UserProfile} /> */}
+                <Drawer.Screen name="User Profile" component={UserProfile} />
                 {/* <Drawer.Screen name="Cuentas" component={Cuentas} /> */}
                 <Drawer.Screen name="Statistics" component={Statistics} />
                 <Drawer.Screen name="Log out" component={LandingPage} />
