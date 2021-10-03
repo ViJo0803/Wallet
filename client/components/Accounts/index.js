5c3deac075e51b6372100db31046f3c15d36657e
Paola Vargas <vrgspaola68@gmail.com>
2021-10-03 13:56:45 -0400

    hello

--- client/components/Accounts/index.js
+++ client/components/Accounts/index.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import { View, Button, Text } from 'react-native';
-
+import React from "react";
+import { View, Button, Text } from "react-native";
 
 function Accounts({ navigation }) {
-    return (
-        <View>
-            <Text>This is Statistics</Text>
-            <Button title="Go Home" onPress={() => navigation.navigate('Home')}/>
-        </View>
-    )
-
+  //estuve por aqui
+  return (
+    <View>
+      <Text>This is Statistics</Text>
+      <Button title="Go Home" onPress={() => navigation.navigate("Home")} />
+    </View>
+  );
 }
 
-export default Accounts
+export default Accounts;
