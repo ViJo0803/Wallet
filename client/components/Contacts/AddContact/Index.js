2da8e5c2d6712344dfba3f737431de768aa6e2c6
Luciano-Fuertes <chiva.fuertes@gmail.com>
2021-10-13 15:47:29 -0300

    push for share

--- client/components/Contacts/AddContact/Index.js
+++ client/components/Contacts/AddContact/Index.js
@@ -1,11 +1,47 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, TextInput, Text, ScrollView, Alert, TouchableOpacity, Button } from 'react-native'
 import { styles } from './styles'
 
 export default function AddContact() {
+
+    const [searchText, setSearchText] = useState('');
+    const [users, setUsers] = useState([{ name: 'uno', phone: 'telefono' }, { name: 'dos', phone: 'telefono' }]);
+
     return (
         <View style={styles.container}>
-            <Text>You're now in Add Contact tab</Text>
+            <TextInput
+                placeholder='Search for contacts...'
+                textContentType="name"
+                style={styles.input}
+                onChange={() => {
+                    setSearchText({ name: 'uno', phone: 'telefono' });
+                }}
+            />
+            <Button
+                title='Search'
+                onPress={() => { setUsers(searchText), console.log(users) }}
+            />
+
+            {users ? (
+                <View>
+                    {(users?.map((e) => (
+                        <View>
+                            <Text>{e.name}</Text>,
+                            <TouchableOpacity
+                                key={e.name}
+                                style={styles.userCard}
+                                title='Add'
+                                onPress={() => {
+                                    Alert.alert(
+                                        `${e.name} is now your contact`
+                                    );
+                                }} />
+                        </View>
+                    )))}
+                </View>) : <View>
+                <Text>No user found</Text>
+            </View>}
         </View>
     )
 }
+
--- client/components/Contacts/AddContact/styles.js
+++ client/components/Contacts/AddContact/styles.js
@@ -3,11 +3,24 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
     container: {
-        height: '100%',
-        width: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: "column",
+        flex: 1,
+        paddingHorizontal: 12,
+        paddingTop: 10,
+        // backgroundImage: 'linear-gradient(180deg, rgba(248,249,250,1) 0%, rgba(173,181,189,1) 100%)',
+        backgroundColor: '#B5FFE1',
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+
+    },
+    userCard: {
+        backgroundColor: "#fafafa",
+        paddingVertical: 6,
+        paddingHorizontal: 6,
+        borderRadius: 10,
+        marginTop: 10,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
     },
 })
