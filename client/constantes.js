e4128ff0d7fb125436eaade04def7c210b78f65e
SViJo0803 <vijo967@gmail.com>
2021-10-19 17:29:35 -0500

    datos

--- client/constantes.js
+++ client/constantes.js
@@ -1,3 +1,5 @@
 // abrir dos pestañas de ngrok, con los comandos: ngrok http 3000 y ngrok http 3001
-export const URL_STRIPE_3000 = "http://604e-181-31-250-253.ngrok.io" // poner aca el url generado por ngrok 3000
-export const URL_API_3001 = "http://36bf-181-31-250-253.ngrok.io" // poner aca el url generado por ngrok 3001
+export const URL_STRIPE_3000 =
+  "http://8304-2800-200-f190-8e7-5546-484e-af5a-9560.ngrok.io"; // poner aca el url generado por ngrok 3000
+export const URL_API_3001 =
+  "http://f45c-2800-200-f190-8e7-5546-484e-af5a-9560.ngrok.io"; // poner aca el url generado por ngrok 3001
