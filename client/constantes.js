3fdef3f55dc138578f17cddbc570657d9e995d90
ViJo0803 <84634814+ViJo0803@users.noreply.github.com>
2021-10-20 13:09:19 -0500

    Update constantes.js

--- client/constantes.js
+++ client/constantes.js
@@ -2,4 +2,4 @@
 export const URL_STRIPE_3000 =
   "http://1cdc-2800-200-f190-8e7-5c5e-4a88-5d34-ed10.ngrok.io"; // poner aca el url generado por ngrok 3000
 export const URL_API_3001 =
-  "http://0ea2-2800-200-f190-8e7-5c5e-4a88-5d34-ed10.ngrok.io"; // poner aca el url generado por ngrok 3001
+  "https://walletmint.herokuapp.com/"; // poner aca el url generado por ngrok 3001
