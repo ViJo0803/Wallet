db2c315f1dd54a6e08dd2981d7c2e96a59cbb01e
Luciano-Fuertes <chiva.fuertes@gmail.com>
2021-10-20 20:27:11 -0300

    corregido el uso del estado

--- client/components/Home/index.js
+++ client/components/Home/index.js
@@ -6,11 +6,12 @@ import { useSelector } from "react-redux";
 function Home() {
 
   const balance = useSelector((state) => state.account.accounts)
+  const state = useSelector((state) => state.user.user)
 
   return (
     <View style={styles.container}>
       <View style={styles.inputView}>
-        <Text style={styles.welcome}>Welcome, {name}</Text>
+        <Text style={styles.welcome}>Welcome, {state.nombre}</Text>
         <Text style={styles.title}>Current balance</Text>
         <Text style={styles.title}>{balance[0]?.saldo || 0} ARS</Text>
       </View>
