71deca4f8719eddd7a25a69ad9f3567ce64c4e14
Agustin Chaves <85836863+AguChaves92@users.noreply.github.com>
2021-10-20 18:46:30 -0300

    Agus-Styles

--- client/components/UserProfile/index.js
+++ client/components/UserProfile/index.js
@@ -1,53 +1,90 @@
 import React from "react";
-import { View, Text, Image, Button } from "react-native";
+import { View, Button } from "react-native";
 import { styles } from "./styles";
 import { useSelector } from "react-redux";
+import {
+  Avatar,
+  Title,
+  Caption,
+  Text,
+  TouchableRipple,
+} from "react-native-paper";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
-
-
-function UserProfile({navigation}) {
-  console.log(navigation);
-
+function UserProfile() {
   const state = useSelector((state) => state.user.user);
+  const balance = useSelector((state) => state.account.accounts);
 
+  console.log("this is the state ", state);
+  return (
+    <View style={styles.container}>
+      <Avatar.Image
+        source={require("../../assets/JimC.jpg")}
+        style={styles.image}
+        size={200}
+      />
 
-    console.log("this is the state ", state)
-    return (
-        <View style={styles.container}>
-            <Image
-                source={state.foto}
-                style={styles.image} />
-            <View style={styles.textBox}>
-                <View>
-                    <Text style={styles.text}>Name: {state.nombre} {state.apellidos}</Text>
-                </View>
-                <View>
-                    <Text style={styles.text}>Mail: {state.mail}</Text>
-                </View>
-                <View>
-                    <Text style={styles.text}>DNI: {state.dni}</Text>
-                </View>
-                <View>
-                    <Text style={styles.text}>Phone: {state.telefono}</Text>
-                </View>
-                <View>
-                    <Text style={styles.text}>CP: {state.codigo_postal}</Text>
-                </View>
-
-                <View>
-                    <Text style={styles.text}>Nickname: {state.nickname}</Text>
-                </View>
-            </View>
-      
-      <View style={styles.containerButtons}>
-        <Button title="show" style={styles.button} />
-        <Button
-          title="edit"
-          style={styles.button}
-          onPress={() => navigation.navigate("EditProfile")}
-        />
-        
+      <View>
+        <Title style={styles.title}>
+          {state.nombre} {state.apellidos}
+        </Title>
+        <View style={styles.adress}>
+          <Icon style={styles.icon} color={"#232020"}  name="map-marker-radius" size={15} />
+          <Caption style={styles.caption}>{state.direccion}</Caption>
+        </View>
+        <View style={styles.adress}>
+          <Icon name="phone" color={"#232020"} size={15} style={styles.icon} />
+          <Caption style={styles.caption}>{state.telefono}</Caption>
+        </View>
+        <View style={styles.adress}>
+          <Icon name="email" color={"#232020"} size={15} style={styles.icon} />
+          <Caption style={styles.caption}>{state.mail}</Caption>
+        </View>
       </View>
+      <View style={styles.wrapperbox}>
+        <View style={styles.infobox}>
+          <Title style={styles.title}>$ {balance[0].saldo}</Title>
+          <Caption style={styles.caption}>Wallet</Caption>
+        </View>
+        <View style={styles.infobox}>
+          <Title style={styles.title}>{balance[0].numerocuenta}</Title>
+          <Caption style={styles.caption}>CVU</Caption>
+        </View>
+      </View>
+
+      <View style={styles.menuWrapper}>
+        <TouchableRipple onPress={()=>console.log("Deberia ir a transfers a ver mis contactos :)")/*</View>navigation,navigate(transfers)*/}>
+        <View style={styles.menuItem}>
+          <Icon style={styles.icon} name="heart-outline"  size={20} color={"#FF6347"}/>
+          <Text style={styles.text}>A favoritos</Text>
+        </View>
+        </TouchableRipple>
+      </View>
+      <View style={styles.menuWrapper}>
+        <TouchableRipple onPress={()=>console.log("Deberia ir a pay services :)")/*</View>navigation,navigate(servicios)*/}>
+        <View style={styles.menuItem}>
+          <Icon style={styles.icon} name="credit-card"  size={20} color={"#FF6347"}/>
+          <Text style={styles.text}>Paga tus servicios</Text>
+        </View>
+        </TouchableRipple>
+      </View>
+      <View style={styles.menuWrapper}>
+        <TouchableRipple onPress={()=>console.log("Deberia ir a Cargar saldo :)")/*</View>navigation,navigate(deposit)*/}>
+        <View style={styles.menuItem}>
+          <Icon style={styles.icon} name="account-check-outline"  size={20} color={"#FF6347"}/>
+          <Text style={styles.text}>Carga Saldo</Text>
+        </View>
+        </TouchableRipple>
+      </View>
+      <View style={styles.menuWrapper}>
+        <TouchableRipple onPress={()=>console.log("Deberia ir a Editar perfil :)")/*</View>navigation,navigate(edituserprofile)*/}>
+        <View style={styles.menuItem}>
+          <Icon style={styles.icon} name="share-outline"  size={20} color={"#FF6347"}/>
+          <Text style={styles.text}>Editar Perfil</Text>
+        </View>
+        </TouchableRipple>
+      </View>
+
     </View>
   );
 }
--- client/constantes.js
+++ client/constantes.js
@@ -1,3 +1,3 @@
 // abrir dos pestañas de ngrok, con los comandos: ngrok http 3000 y ngrok http 3001
-export const URL_STRIPE_3000 = "http://742f-181-171-111-218.ngrok.io" // poner aca el url generado por ngrok 3000
-export const URL_API_3001 = "http://1355-181-171-111-218.ngrok.io" // poner aca el url generado por ngrok 3001
+export const URL_STRIPE_3000 = "http://192.168.0.121:3000" // poner aca el url generado por ngrok 3000
+export const URL_API_3001 = "http://192.168.0.121:3001" // poner aca el url generado por ngrok 3001
