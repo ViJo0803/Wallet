60e7244e021f5589d54065f216f1aaef4d417292
SViJo0803 <vijo967@gmail.com>
2021-10-20 23:14:51 -0500

    lib

--- client/components/Coins/index.js
+++ client/components/Coins/index.js
@@ -1,17 +1,103 @@
-import React from "react";
-import { View, Button, Text, Image } from "react-native";
-import { styles } from "./styles";
+// import { styles } from "./styles";
+
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button, Image } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import QRCode from "react-native-qrcode-svg";
 
 function Coins({ navigation }) {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState("Not yet scanned");
+
+  const askForCameraPermission = () => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  };
+
+  // Request Camera Permission
+  useEffect(() => {
+    askForCameraPermission();
+  }, []);
+
+  // What happens when we scan the bar code
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data);
+    console.log("Type: " + type + "\nData: " + data);
+  };
+
+  // Check permissions and return the screens
+  if (hasPermission === null) {
+    return (
+      <View style={styles.container}>
+        <Text>Requesting for camera permission</Text>
+      </View>
+    );
+  }
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ margin: 10 }}>No access to camera</Text>
+        <Button
+          title={"Allow Camera"}
+          onPress={() => askForCameraPermission()}
+        />
+      </View>
+    );
+  }
+
   return (
+    // <View style={styles.container}>
+    //   <Text style={styles.text}>
+    //     Coins component currently on building stage
+    //   </Text>
+    //   <Image source={require("../../assets/Coins.png")} style={styles.image} />
+    //   <Button title="Go Home" onPress={() => navigation.navigate("Home")} />
+    // </View>
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Coins component currently on building stage
-      </Text>
-      <Image source={require("../../assets/Coins.png")} style={styles.image} />
-      <Button title="Go Home" onPress={() => navigation.navigate("Home")} />
+      <View style={styles.barcodebox}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={{ height: 400, width: 400 }}
+        />
+      </View>
+      <Text style={styles.maintext}>{text}</Text>
+
+      {scanned && (
+        <Button
+          title={"Scan again?"}
+          onPress={() => setScanned(false)}
+          color="tomato"
+        />
+      )}
+      <QRCode value="http://awesome.link.qr" />
     </View>
   );
 }
 
 export default Coins;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  maintext: {
+    fontSize: 16,
+    margin: 20,
+  },
+  barcodebox: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 300,
+    width: 300,
+    overflow: "hidden",
+    borderRadius: 30,
+    backgroundColor: "tomato",
+  },
+});
--- client/constantes.js
+++ client/constantes.js
@@ -1,7 +1,5 @@
 // abrir dos pestañas de ngrok, con los comandos: ngrok http 3000 y ngrok http 3001
 
 export const URL_STRIPE_3000 =
-  "http://192.168.1.114:3000"; // poner aca el url generado por ngrok 3000
-export const URL_API_3001 =
-  "https://walletmint.herokuapp.com"; // poner aca el url generado por ngrok 3001
-
+  "http://04ca-2800-200-f190-8e7-5c5e-4a88-5d34-ed10.ngrok.io"; // poner aca el url generado por ngrok 3000
+export const URL_API_3001 = "https://walletmint.herokuapp.com"; // poner aca el url generado por ngrok 3001
