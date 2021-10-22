39a61a487228736f95ec66ac9d2beb8c312f62ee
Paola Vargas <vrgspaola68@gmail.com>
2021-10-22 08:23:10 -0400

    feature styles in home

--- client/components/Home/styles.js
+++ client/components/Home/styles.js
@@ -2,81 +2,103 @@ import { StyleSheet } from "react-native";
 import { colors } from "../../utils/colors";
 
 export const styles = StyleSheet.create({
-  container: {
+  bigBox: {
+    display: "flex",
+    justifyContent: "space-between",
     height: "100%",
+  },
+  headerContainer: {
     width: "100%",
+    backgroundColor: colors.brand,
     display: "flex",
-    backgroundColor: "white",
-    flexDirection: "column",
+    padding: 20,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    shadowColor: colors.tertiary,
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.0,
+
+    elevation: 24,
   },
-  buttonGrid: {
-    height: "68%",
+  buttonCardContainer: {
     display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-    flexWrap: "wrap",
-  },
-  buttonCard: {
-    marginVertical: "10%",
-    marginHorizontal: "15%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  cuentas: {
-    height: "15%",
-    justifyContent: "space-around",
     flexDirection: "row",
+    flexWrap: "wrap",
+    marginRight: "7%",
+    marginLeft: "5%",
+    paddingBottom: 60,
+    alignContent: "center",
+    justifyContent: "space-between",
+  },
+  shortcuts: {
+    fontWeight: "500",
+    fontSize: 13,
+    textTransform: "uppercase",
+    color: colors.tertiary,
+    marginLeft: 18,
+    marginTop: 20,
+  },
+
+  buttonCard: {
+    width: "45%",
+    padding: "5%",
+    shadowColor: colors.tertiary, // IOS
+    shadowOffset: { height: 10, width: 6 }, // IOS
+    shadowOpacity: 6, // IOS
+    shadowRadius: 6, //IOS
+    backgroundColor: colors.secondary,
+    elevation: 8, // Android
+    alignItems: "center",
+    marginBottom: 10,
+    justifyContent: "space-around",
+    borderRadius: 5,
+    margin: 5,
   },
   welcome: {
-    fontSize: 30,
-    fontWeight: "700",
+    fontSize: 20,
+    fontWeight: "500",
     color: colors.primary,
   },
   title: {
     fontWeight: "700",
-    fontSize: 25,
+    fontSize: 18,
     textTransform: "uppercase",
-    color: colors.primary,
+    color: colors.green,
+    marginTop: 10,
   },
-  buttonIngresar: {
-    margin: "5%",
+  balance: {
+    fontWeight: "700",
+    fontSize: 35,
+    textTransform: "uppercase",
+    color: colors.green,
   },
-  titleTransfer: {
-    fontWeight: "bold",
+
+  shortcutsTitle: {
+    fontWeight: "400",
+    fontSize: 12,
+    textTransform: "uppercase",
+    color: colors.tertiary,
+    textAlign: "center",
   },
-  scrollTransfer: {
-    width: "100%",
-  },
-  inputView: {
-    width: "100%",
-    backgroundColor: colors.brand,
-    display: "flex",
-    height: "27%",
-    padding: 20,
-    borderBottomLeftRadius: 30,
-    borderBottomRightRadius: 30,
-  },
-  textTransf: {
-    backgroundColor: "#2f3542",
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  userCard: {
-    width: "100%",
-    backgroundColor: "#2f3542",
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  textname: {
-    fontWeight: "500",
-    color: "#fff",
-  },
-  textdate: {
-    alignContent: "flex-end",
-    color: "#fff",
-  },
+  // viewScrollStyles: {
+  //   display: "flex",
+  //   flexDirection: "row",
+  //   flexWrap: "wrap",
+  //   marginRight: "5%",
+  //   marginLeft: "5%",
+  //   marginBottom: "40%",
+  // },
+  // imageScroll: {
+  //   resizeMode: "cover",
+  //   width: null,
+  //   height: null,
+  // },
+  // bannerContainer: {
+  //   display: "flex",
+  //   flexWrap: "wrap",
+  // },
 });
